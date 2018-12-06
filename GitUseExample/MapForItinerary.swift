9437a01b3f9771b91bebb7f30447e5b7459726a9
李泽昆 <844327426@qq.com>
2018-12-06 13:19:34 +0800

    Add new map-foritinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by lizekun on 2018/12/6.
+//  Copyright © 2018 Frank. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
